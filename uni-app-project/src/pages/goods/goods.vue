4f7b2d52e14b6017898d91fe16e9dbd36efd4a46
HWJ <hwj20010504@163.com>
2023-08-29 16:20:47 +0800

    实现轮播图交互:图片预览+数字显示

--- uni-app-project/src/pages/goods/goods.vue
+++ uni-app-project/src/pages/goods/goods.vue
@@ -2,6 +2,7 @@
 import { getGoodsByIdAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
 import { onLoad } from '@dcloudio/uni-app'
+import { computed } from 'vue'
 import { ref } from 'vue'
 
 // 获取屏幕边界到安全区域距离
@@ -19,6 +20,19 @@ const getGoodsByIdData = async () => {
   goods.value = result
 }
 
+// 轮播图交互
+const currentImage = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currentImage.value = ev.detail.current
+}
+// 大图预览
+const onTapImage = (url: string) => {
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures
+  })
+}
+
 onLoad(() => {
   getGoodsByIdData()
 })
@@ -30,15 +44,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper circular @change="onChange">
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image mode="aspectFill" :src="item" @tap="onTapImage(item)" />
           </swiper-item>
         </swiper>
-        <view class="indicator">
-          <text class="current">1</text>
+        <view class="indicator" v-if="goods?.id">
+          <text class="current">{{ currentImage + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
