774a2c99038dd4c0cf2fa41dde697353d5e19cbb
HWJ <hwj20010504@163.com>
2023-08-30 11:44:27 +0800

    用户退出登录功能

--- uni-app-project/src/pagesMember/settings/settings.vue
+++ uni-app-project/src/pagesMember/settings/settings.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
-//
+import { useMemberStore } from '@/stores'
+
+const memberStore = useMemberStore()
+// 用户退出登录
+const onLogout = () => {
+  // 模态弹窗
+  uni.showModal({
+    content: '是否退出登录？',
+    showCancel: true,
+    success: ({ confirm }) => {
+      if (confirm) {
+        // 确认退出登录
+        memberStore.clearProfile()
+        uni.navigateBack()
+      } else {
+        // 取消退出登录
+      }
+    }
+  })
+}
 </script>
 
 <template>
   <view class="viewport">
     <!-- 列表1 -->
-    <view class="list" v-if="true">
+    <view class="list" v-if="memberStore.profile">
       <navigator url="/pagesMember/address/address" hover-class="none" class="item arrow">
         我的收货地址
       </navigator>
@@ -21,8 +40,8 @@
       <navigator hover-class="none" class="item arrow" url=" ">关于小兔鲜儿</navigator>
     </view>
     <!-- 操作按钮 -->
-    <view class="action">
-      <view class="button">退出登录</view>
+    <view class="action" v-if="memberStore.profile">
+      <view class="button" @tap="onLogout">退出登录</view>
     </view>
   </view>
 </template>
