541d3fd8d8a106c1f37b2e4bbaddd122f2951173
HWJ <hwj20010504@163.com>
2023-09-14 16:47:44 +0800

    添加icon

--- password_manage_client/src/views/layout/LayoutContainer.vue
+++ password_manage_client/src/views/layout/LayoutContainer.vue
@@ -1,21 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Lock, Document, Setting } from '@element-plus/icons-vue'
+</script>
 
 <template>
   <el-container class="layout-container">
     <!-- 侧边导航栏 -->
     <el-aside width="200px">
       <div class="el-aside _logo">这里是Logo</div>
-      <el-menu active-text-color="#eff5ff" default-active="2" text-color="#606266" router>
-        <el-menu-item>
-          <el-icon></el-icon>
+      <el-menu active-text-color="#7ab0fc" default-active="2">
+        <el-menu-item index="1">
+          <el-icon><Lock /></el-icon>
           <span>密码</span>
         </el-menu-item>
-        <el-menu-item>
-          <el-icon></el-icon>
+        <el-menu-item index="2">
+          <el-icon><Document /></el-icon>
           <span>一般</span>
         </el-menu-item>
-        <el-menu-item>
-          <el-icon></el-icon>
+        <el-menu-item index="3">
+          <el-icon><Setting /></el-icon>
           <span>设置</span>
         </el-menu-item>
       </el-menu>
@@ -40,6 +42,12 @@
   ._log {
     height: 120px;
     // background: url('@/assets/logo.png') no-repeat center / 120px auto;
+  }
+  .el-menu {
+    border-right: none;
+    .el-menu-item {
+      font-size: large;
+    }
   }
 }
 </style>
