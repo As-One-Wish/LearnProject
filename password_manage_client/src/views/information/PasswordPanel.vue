3b345a0e9ef4657c0b1494b6c717617f51bc5f7d
HWJ <hwj20010504@163.com>
2023-09-18 20:13:28 +0800

    实现InfoTable组件的头部设计

--- password_manage_client/src/views/information/PasswordPanel.vue
+++ password_manage_client/src/views/information/PasswordPanel.vue
@@ -3,7 +3,11 @@ import InfoTable from '@/views/information/components/InfoTable.vue'
 </script>
 
 <template>
-  <InfoTable></InfoTable>
+  <InfoTable>
+    <template #title>
+      <span style="font-weight: bold; letter-spacing: 2px">密码信息</span>
+    </template>
+  </InfoTable>
 </template>
 
 <style scoped></style>
--- password_manage_client/src/views/information/components/InfoTable.vue
+++ password_manage_client/src/views/information/components/InfoTable.vue
@@ -1,31 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Search } from '@element-plus/icons-vue'
+</script>
 <template>
-  <el-container class="layout-container">
-    <el-header>
-      <el-row>
-        <el-col :span="8">你好</el-col>
-        <el-col :span="8"></el-col>
-        <el-col :span="8">你好</el-col>
-      </el-row>
-    </el-header>
-    <el-main>Main</el-main>
-  </el-container>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <slot name="title"></slot>
+        <el-input placeholder="查找信息" :prefix-icon="Search"></el-input>
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+  </el-card>
 </template>
 <style scoped lang="less">
-.layout-container {
+.box-card {
   border-radius: 15px;
   height: 100%;
   background-color: #ffffff;
-  .el-header {
-    height: 20%;
-    .el-row {
-      background-color: aqua;
-      align-items: center;
-      justify-content: space-between;
-    }
-  }
-  .el-main {
+  .card-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     height: 80%;
+    .el-input {
+      width: 200px;
+      :deep(.el-input__wrapper) {
+        border-radius: 20px;
+      }
+    }
   }
 }
 </style>
