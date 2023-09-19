01ce07825de5f841bd45ed0e405df9628cf25f4e
HWJ <hwj20010504@163.com>
2023-09-19 22:13:40 +0800

    图标微调

--- password_manage_client/src/views/layout/LayoutContainer.vue
+++ password_manage_client/src/views/layout/LayoutContainer.vue
@@ -6,14 +6,15 @@ import HeaderComponent from '@/components/HeaderComponent.vue'
 <template>
   <el-container class="layout-container">
     <!-- 侧边导航栏 -->
-    <el-aside width="70px">
+    <el-aside width="80px">
       <div class="_logo"></div>
       <el-menu active-text-color="#5077f4" default-active="/info" text-color="#797879" router>
         <el-menu-item index="/info">
-          <el-icon size="25"><Lock /></el-icon>
+          <!-- <el-icon size="25"><Lock /></el-icon> -->
+          <Lock style="width: 2em" />
         </el-menu-item>
         <el-menu-item index="/settings">
-          <el-icon size="25"><Setting /></el-icon>
+          <Setting style="width: 2em" />
         </el-menu-item>
       </el-menu>
     </el-aside>
@@ -40,14 +41,14 @@ import HeaderComponent from '@/components/HeaderComponent.vue'
   .el-aside {
     border-right: 1px solid #ebeced;
     ._logo {
-      height: 45px;
+      height: 50px;
       background: url('@/assets/pkq.jpg') no-repeat center;
       background-size: cover;
     }
     .el-menu {
       margin-top: 15px;
       border-right: none;
-      padding-right: 5px;
+      padding-right: 8px;
     }
   }
   .el-header {
