bd607481af291d04a8e737f4ca282a188873a614
HWJ <hwj20010504@163.com>
2024-01-31 17:02:54 +0800

    清空Main

--- Entity.py
+++
@@ -1,9 +0,0 @@
-class MovieInformation:
-    def __init__(self, name, category, release_area, duration, release_date, synopsis, rate):
-        self.name = name
-        self.category = category
-        self.release_area = release_area
-        self.duration_minutes = duration
-        self.release_date = release_date
-        self.synopsis = synopsis
-        self.rate = rate
--- first_spider_demo/main.py
+++
@@ -1,5 +0,0 @@
-import funs
-from constant import CONSTANT
-
-if __name__ == '__main__':
-    per_result = funs.get_per_page_infos(CONSTANT.SPIDER_SSR_URL_1)
